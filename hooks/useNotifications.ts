13f29d526b22c0f7e48abe80ebcead66b47c8d79
Derek Stephen McLean <delboy1978uk@gmail.com>
2024-08-08 18:35:26 +0200

    import statement

--- hooks/useNotifications.ts
+++ hooks/useNotifications.ts
@@ -2,6 +2,7 @@ import * as Device from "expo-device";
 import * as Notifications from "expo-notifications";
 import {useEffect} from "react";
 import {Platform} from "react-native";
+import Constants from "expo-constants";
 
 import expoPushTokensApi from "../api/notifications";
 import {useStorageState} from "./useStorageState";
