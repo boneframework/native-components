02e106720c617a54bfb702d87b54d5db85a4e7dd
Derek Stephen McLean <delboy1978uk@gmail.com>
2024-08-08 18:25:13 +0200

    use one api file

--- api/expoPushTokens.js
+++
@@ -1,7 +0,0 @@
-import client from './client';
-
-const register = pushToken => client.post('/api/notifications/register-token', {token: pushToken});
-
-export default {
-    register,
-}
--- hooks/useNotifications.ts
+++ hooks/useNotifications.ts
@@ -1,10 +1,14 @@
 import * as Device from "expo-device";
 import * as Notifications from "expo-notifications";
-import expoPushTokensApi from "../api/expoPushTokens";
 import {useEffect} from "react";
-import router from "../navigation/rootNavigation";
+import {Platform} from "react-native";
+
+import expoPushTokensApi from "../api/notifications";
+import {useStorageState} from "./useStorageState";
 
 export default useNotifications = (notificationReceivedListener = notification => {}) => {
+    const [[isPushTokenLoading, pushToken], setPushToken] = useStorageState('pushToken');
+
     useEffect(() => {
         registerForPushNotifications();
         Notifications.addNotificationReceivedListener(notification => {
@@ -13,6 +17,17 @@ export default useNotifications = (notificationReceivedListener = notification =
     }, []);
 
     const registerForPushNotifications = () => registerForPushNotificationsAsync();
+
+    const handleRegistrationError = (errorMessage: string) => {
+        alert(errorMessage);
+        throw new Error(errorMessage);
+    }
+
+    const projectId = Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
+
+    if (!projectId) {
+        handleRegistrationError('Project ID not found');
+    }
 
     const registerForPushNotificationsAsync = async () => {
         if (Device.isDevice) {
@@ -26,8 +41,15 @@ export default useNotifications = (notificationReceivedListener = notification =
             if (finalStatus !== 'granted') {
                 return;
             }
-            const token = (await Notifications.getExpoPushTokenAsync()).data;
-            expoPushTokensApi.register(token);
+            const token = (await Notifications.getExpoPushTokenAsync({
+                projectId,
+            })).data;
+
+            if (!isPushTokenLoading && pushToken !== token) {
+                setPushToken(token)
+                expoPushTokensApi.register(token);
+            }
+
         } else {
             alert('Must use physical device for Push Notifications');
         }
