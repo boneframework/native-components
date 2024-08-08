df21f61a2965d571c90a9ec74eb2c15030fca8f1
Derek Stephen McLean <delboy1978uk@gmail.com>
2024-08-08 20:10:42 +0200

    use location, and reract-native-progress

---
+++ screens/UploadScreen.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {Modal, StyleSheet, View} from "react-native";
+import * as Progress from 'react-native-progress';
+
+import colors from '../../../../config/colors'
+import Text from '../components/Text'
+import Animation from "../components/Animation";
+
+function UploadScreen({onDone, progress = 0, visible = false}) {
+    return (
+        <Modal visible={visible}>
+            <View style={styles.container}>
+                { progress < 1
+                    ? <Progress.Bar color={colors.primary} progress={progress} width={200}/>
+                    : <Animation
+                        autoPlay={true}
+                        loop={false}
+                        onAnimationFinish={onDone}
+                        source={require('../assets/animations/done.json')}
+                        style={styles.animation}
+                    />
+                }
+            </View>
+        </Modal>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: "center",
+        flex: 1,
+        justifyContent: "center",
+    },
+    animation: {
+        width: 150,
+    },
+})
+
+export default UploadScreen;
