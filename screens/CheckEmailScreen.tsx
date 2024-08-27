c2787016573427f8829fc9a2e22e05af4f0c7ee5
Derek Stephen McLean <delboy1978uk@gmail.com>
2024-08-27 16:57:46 +0200

    add activate  user screen

---
+++ screens/CheckEmailScreen.tsx
@@ -0,0 +1,59 @@
+import {ImageBackground, StyleSheet, View} from "react-native";
+import React from "react";
+
+import Text from '../components/Text';
+import Animation from "../components/Animation";
+
+import colors from "../../../../config/colors";
+
+function CheckEmailScreen(props) {
+
+    return (
+        <ImageBackground blurRadius={10} style={styles.container} source={require('../../../../assets/background.png')} >
+            <View style={styles.animationContainer}>
+                <Animation
+                    autoPlay={true}
+                    loop={true}
+                    source={require('../../../../assets/animations/email.json')}
+                    style={styles.animation}
+                    speed={1}
+                />
+                <Text style={styles.activate}>Activate your account</Text>
+                <Text style={styles.info}>Check your email and click on the link to open the app and activate your account.</Text>
+            </View>
+        </ImageBackground>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 20
+    },
+    animationContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        paddingHorizontal: 10,
+        marginTop: -50
+    },
+    animation: {
+        width: 150,
+        height: 150,
+    },
+    activate: {
+        marginTop: 20,
+        textTransform: 'uppercase',
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+        color: colors.white,
+        textAlign: 'center'
+    },
+    info: {
+        color: colors.white,
+        textAlign: 'center'
+    },
+})
+
+export default CheckEmailScreen;
