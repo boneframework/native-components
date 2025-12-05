47f625779be13af8d0a403dc5a79278d3a06de7f
Derek Stephen McLean <delboy1978uk@gmail.com>
2025-12-05 16:46:36 +0100

    more typescript upgrades

---
+++ contexts/theme.ts
@@ -0,0 +1,33 @@
+import { createContext } from 'react';
+
+type ThemeColors = {
+    primary: string;
+    secondary: string;
+    black: string;
+    white: string;
+    light: string;
+    whitish: string;
+    medium: string;
+    darkish: string;
+    dark: string;
+    danger: string;
+    bgGradient: string[];
+};
+
+const defaultTheme: ThemeColors = {
+    primary: '#fc5c65',
+    secondary: '#4ecdc4',
+    black: '#000',
+    white: '#fff',
+    light: '#f0f4f4',
+    whitish: '#ddd',
+    medium: '#999',
+    darkish: '#333',
+    dark: '#222',
+    danger: '#ff5200',
+    bgGradient: ['#001C50', '#003698']
+};
+
+const ThemeContext = createContext<ThemeColors>(defaultTheme);
+
+export default ThemeContext;
--- hooks/useNavigationTheme.ts
+++ hooks/useNavigationTheme.ts
@@ -1,8 +1,9 @@
 import { DarkTheme, DefaultTheme } from "@react-navigation/native";
 
+import { ThemeColors } from "@boneframework/native-components/contexts/theme";
 import useStyle from "@boneframework/native-components/hooks/useStyle";
 
-const useNavigationTheme = () => {
+const useNavigationTheme = (colors: ThemeColors) => {
     const style = useStyle();
     const theme = style.dark ? DarkTheme : DefaultTheme
     return {
