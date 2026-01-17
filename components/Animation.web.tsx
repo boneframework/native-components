4fa53416899e9c1f2c5095eaf78c87d3119d9471
Derek Stephen McLean <delboy1978uk@gmail.com>
2026-01-17 14:02:00 +0100

    update web animation

--- components/Animation.web.tsx
+++ components/Animation.web.tsx
@@ -1,23 +1,20 @@
 import React, {useRef} from 'react';
-import DotLottie, {}  from '@lottiefiles/dotlottie-react';
+import Lottie  from 'lottie-react';
 
 function Animation({source, style  = {}, onAnimationFinish = () => {}, autoPlay = true, loop = true, speed = 1.5}) {
-    
-    const lottieRef = useRef<DotLottie>(null);
 
-    // @todo: Fix web DotLottie usage
-    // <DotLottie
-    //         source={source}
-    //         autoPlay={autoPlay}
-    //         loop={loop}
-    //         style={style}
-    //         speed={speed}
-    //         onAnimationFinish={onAnimationFinish}
-    //         ref={lottieRef}
-    //     />
+    const lottieRef = useRef<Lottie>(null);
 
-    return(<></>
-        
+    return(
+        <Lottie
+            source={source}
+            autoPlay={autoPlay}
+            loop={loop}
+            style={style}
+            speed={speed}
+            onAnimationFinish={onAnimationFinish}
+            ref={lottieRef}
+        />
     );
 }
 
